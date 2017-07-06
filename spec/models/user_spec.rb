334b4a3b148096f367e19bdb2605ac3e276e2019
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-06 17:14:39 +0530

    added test for new user subscription tier and approval

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model  do
+  let(:user) { User.new }
 
   it { should have_many :reports }
   it { should have_many :buzzs }
   it { should have_many(:locations).through(:reports) }
   it { should validate_presence_of :state_waters }
 
+  it { expect(user.approved).to be false }
+  it { expect(user.subscription_tier).to eq 'recreational' }
 end
